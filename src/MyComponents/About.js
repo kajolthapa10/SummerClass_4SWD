756e3a505c4dfa37f45866dcaa451614b5b57b26
kajolthapa10 <bs3261066@gmail.com>
2026-07-14 12:19:24 +0545

    my first react project

---
+++ src/MyComponents/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About=()=>{
+    return(
+
+        <div className="conatiner my-5">
+            <h2>
+                About my Todo List
+            </h2>
+
+            <p>
+                This is a simple todo list project made with reactjs where i can dd todo and delete the todo.
+            </p>
+
+            <p>
+                Feature available only -Add, view, and Delete.
+            </p>
+        </div>
+    );
+};
+
+export {About};
+//export default App;
